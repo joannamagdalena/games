5f519f0f2dd6b6f7cef46eb58956ac24d8a200cc
Joanna <joannakaczmarek93@gmail.com>
2025-03-08 16:19:49 +0100

    small fixes

--- dynamic_probabilistic_penrose_banzhaf.py
+++ dynamic_probabilistic_penrose_banzhaf.py
@@ -8,7 +8,7 @@ import math
 def count_winning_coalitions(number_of_players, player_weights, weight_of_N, quota):
     T = np.zeros(weight_of_N)
     T[weight_of_N - 1] = 1
-    for i in range(number_of_players+1):
+    for i in range(1, number_of_players+1):
         if quota + player_weights[i-1] <= weight_of_N:
             for x in range(quota + player_weights[i-1], weight_of_N + 1):
                 T[x - player_weights[i-1] - 1] += T[x - 1]
@@ -18,7 +18,7 @@ def count_winning_coalitions(number_of_players, player_weights, weight_of_N, quo
 # number of winning coalitions containing a player
 def count_winning_coalitions_with_player(number_of_players, player_weights, weight_of_N, quota, T):
     Tw = np.zeros((number_of_players, weight_of_N))
-    for i in range(number_of_players + 1):
+    for i in range(1, number_of_players + 1):
         for x in range(weight_of_N - player_weights[i-1] + 1, weight_of_N + 1):
             Tw[i - 1, x - 1] = T[x - 1]
         for x in reversed(range(quota, weight_of_N - player_weights[i-1] + 1)):
@@ -35,18 +35,13 @@ def probabilistic_PBI(game):
 
     T = count_winning_coalitions(number_of_players, player_weights, weight_of_N, quota)
     Tw = count_winning_coalitions_with_player(number_of_players, player_weights, weight_of_N, quota, T)
-    print(Tw)
 
     indices = []
     denominator = math.pow(2, number_of_players - 1)
-    for i in range(number_of_players+1):
+    for i in range(1, number_of_players+1):
         pbi = 0
         for x in range(quota, min(quota + player_weights[i-1], weight_of_N+1)):
             pbi += Tw[i - 1, x - 1] / denominator
         indices.append(pbi)
 
     return indices
-
-g = [[13, 6, 14, 12, 1], 43]
-g2 = [[1,2,2,2,4],8]
-print(probabilistic_PBI(g2))
